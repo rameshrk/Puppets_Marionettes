e4daf05226a1f4b4a6ab22f603029e3a14f3c0a5
git <git@demo.(none)>
2013-09-16 02:43:38 +0530

    Sixth change

--- manifests/nodesusercreate.pp
+++ manifests/nodesusercreate.pp
@@ -5,5 +5,10 @@ comment => 'Art Vandelay',
 home => '/home/art',
 managehome => true,
 }
+ssh_authorized_key { 'art_ssh':
+user => 'art',
+type => 'rsa',
+key => AAAAB3NzaC1yc2EAAAABIwAAAQEAr2r++ngsyuClQdvWqOAUWkUGt0YdPziClHAq9EzEYZK3ZhHhy1CFB0jL/d6GX0YA/jcRrp0qLQJnNLze47F2lUwM+P4dNLgHHpsS+wYI99ZQ5Q8o6UxA8YrX455WZdFtn9xMtiiH7etOkD1vvejlAQ4kJ5gU5KQUDUJOu/Fd7q1+R48snmM9Ddj/j8uc2npnQbtrjktlYTJzRBhYFb8WENxMcnBkSi+bnWHYitPSY0mFfmqr6g6co+FAkyyKpkMDDld6IY/JAQpDJSUgry7LFaGwyQhN+wGZ5vC43k0Tlc0qa/A4baPz/ux3WzS2ZCTi3sgScPPStarKWNWUzaPiyQ==',
+}
 }
 
