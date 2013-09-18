e8cf22bca33e13b36726af1a3be0adc4d35b8453
Ramesh Nair <rameshrk.coder@gmail.com>
2013-09-18 17:00:02 +0530

    Template change

--- manifests/nodestemplatenginx.pp
+++ manifests/nodestemplatenginx.pp
@@ -6,5 +6,14 @@ file { '/etc/nginx/sites-enabled/cat-pictures.conf':
 content => template('nginx/vhost.conf.erb'),
 notify => Service['nginx'],
 }
+file { '/tmp/the_answer.txt':
+content => inline_template("What do you get if you multiply six by nine? <%= 6 * 7 %>.\n")
+}
+
+file { '/tmp/the_facter.txt':
+content => inline_template("My address is <%= @ipaddress %>.\n")
+}
+
+
 }
 
