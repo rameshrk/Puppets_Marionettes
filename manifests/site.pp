b14ebfbebcbd97004221ffbe04baefa8c8dc3ce3
Ramesh Nair <rameshrk.coder@gmail.com>
2013-09-18 14:21:55 +0530

    Template Change

--- manifests/site.pp
+++ manifests/site.pp
@@ -1,4 +1,5 @@
-import 'nodesdistribute.pp'
+import 'nodestemplatenginx.pp'
+#import 'nodesdistribute.pp'
 #import 'nodesbackup.pp'
 #import 'nodesexec.pp'
 #import 'nodesusercreate.pp'
--- modules/nginx/manifests/init.pp
+++ modules/nginx/manifests/init.pp
@@ -2,11 +2,13 @@ class nginx {
 package { 'nginx':
 ensure => installed,
 }
+
 service { 'nginx':
 ensure => running,
 enable => true,
 require => Package['nginx'],
 }
+
 file { '/etc/nginx/sites-enabled/default':
 ensure => absent,
 }
